8b907ea0f17d611beeb4caceee1d7404e8f19542
lacsina-sir <152266243+lacsina-sir@users.noreply.github.com>
2024-10-12 21:02:40 +0800

    Add files via upload

---
+++ activity1__php.php
@@ -0,0 +1,18 @@
+<?php
+echo "Numbers from 1 to 10:\n";
+$counter = 1;
+while ($counter <= 10) {
+    echo $counter . "\n";
+    $counter++;
+}
+
+echo "\n"; 
+
+
+echo "Even numbers from 1 to 20:\n";
+$counter = 2;
+while ($counter <= 20) {
+    echo $counter . "\n";
+    $counter += 2;
+}
+?>
